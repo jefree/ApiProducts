ba5f3f5d10050f5b83d933fca7ec655881d5b872
Cristian Benitez <clbenitez64@gmail.com>
2016-11-17 18:31:15 -0500

    add cors plugin

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 
   "github.com/astaxie/beego/orm"
   _ "github.com/lib/pq"
+  "github.com/astaxie/beego/plugins/cors"
 )
 
 func configDB() {
   orm.RegisterDriver("postgres", orm.DRPostgres)
-  orm.RegisterDataBase("default", "postgres", "postgres://advocates:aguacat3s@localhost:5432/products")
+  orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("db_config"))
 }
 
 func main() {
@@ -19,6 +20,20 @@ func main() {
     beego.BConfig.WebConfig.DirectoryIndex = true
     beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
   }
+  // CORS for https://foo.* origins, allowing:
+  // - PUT and PATCH methods
+  // - Origin header
+  // // - Credentials share
+
+  beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options {
+    AllowOrigins: []string{"*"},
+    AllowMethods: []string{"PUT", "PATCH"},
+    AllowHeaders: []string{"Origin"},
+    ExposeHeaders: []string{"Content-Length"},
+    AllowCredentials: true,
+  }))
+
+
   configDB()
   beego.Run()
 }
